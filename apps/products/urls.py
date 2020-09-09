f32b51e150432a92b92f1a03b0eb219816a3e0e8
l-mwarangu <lmwa587@gmail.com>
2020-09-10 00:32:16 +0300

    deployment

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -9,4 +9,5 @@ from .views import (
 urlpatterns = [
     url(r'^$', ProductListView.as_view(), name='list'),
     url(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
+    url(r'^(?P<slug>[\w-]+)/(?P<pk>\d+)/$', ProductDownloadView.as_view(), name='download'),
 ]
